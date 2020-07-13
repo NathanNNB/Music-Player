467b06611bf6b82102e3d8427e141d4e3bf859b4
NathanNNB <nathan-borges2011@live.com>
2020-07-13 00:32:00 -0300

    Commit 1
    Criando estrutura do layout

---
+++ data.js
@@ -0,0 +1,21 @@
+window.audios = [
+{
+title:"Warriors",
+artist: "Dan Reynolds",
+cover: "warriors.jpg",
+file: "warriors.wav"
+},
+{
+    title:"Odisseia",
+    artist: "League of Legends",
+    cover: "odisseia.jpg",
+    file: "odisseia.mp3"
+},
+{
+    title:"Gangplank Theme",
+    artist: "League of Legends",
+    cover: "gangplank.jpg",
+    file: "gangplank.wav"
+}
+
+];
